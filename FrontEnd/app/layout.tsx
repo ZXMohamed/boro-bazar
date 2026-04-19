c76384edf4a5d849441d3408913fd826360c47b9
ZXMoahmed <mhmdalshwamy@outlook.com>
2026-04-19 19:42:27 +0200

    fix productCard type naming + common files naming

--- FrontEnd/app/layout.tsx
+++ FrontEnd/app/layout.tsx
@@ -1,5 +1,5 @@
-import Footer from "@/components/layout/footer";
-import Navbar from "@/components/layout/navbar";
+import Footer from "@/components/layout/Footer";
+import Navbar from "@/components/layout/Navbar";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
--- FrontEnd/components/home/topCategories/TopCategories.tsx
+++ FrontEnd/components/home/topCategories/TopCategories.tsx
@@ -1,4 +1,4 @@
-import { categories } from "@/components/layout/navbar";
+import { categories } from "@/components/layout/Navbar";
 import { Button } from "@/components/ui/button";
 import {
   Carousel,
