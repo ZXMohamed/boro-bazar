33253d2b2ef4809fc69823324b7f1238b9470df7
ZXMoahmed <mhmdalshwamy@outlook.com>
2026-04-19 21:02:29 +0200

    order list + address list

--- FrontEnd/app/checkout/page.tsx
+++ FrontEnd/app/checkout/page.tsx
@@ -1,5 +1,13 @@
+import AddressList from "@/components/checkout/AddressList";
+import OrderList from "@/components/checkout/OrderList";
+
 const CheckoutPage = () => {
-  return <div>CheckoutPage</div>;
+  return (
+    <section className="container m-auto flex justify-between gap-x-2 gap-y-5 px-5 py-5 max-lg:flex-wrap sm:px-10 sm:py-10 2xl:px-20">
+      <AddressList />
+      <OrderList />
+    </section>
+  );
 };
 
 export default CheckoutPage;
