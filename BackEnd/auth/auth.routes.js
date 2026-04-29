ef4d73c9c31a92c759033b453db039d0163d7120
AyshaVolidis <ayshavolidis@gmail.com>
2026-04-29 16:21:37 +0300

    feat: implement authentication routes and protected access middleware

--- BackEnd/auth/auth.routes.js
+++ BackEnd/auth/auth.routes.js
@@ -1,4 +1,5 @@
 import express from "express";
+
 import {
   register,
   login,
@@ -12,12 +13,15 @@ import {
   verifyOtp,
   resetPassword,
 } from "./auth.controller.js";
+
 import { protect } from "../middleware/authmiddleware.js";
 
 const router = express.Router();
 
+/* Auth Routes */
 router.post("/signup", register);
 router.post("/login", login);
+
 router.post("/refresh-token", refreshTokens);
 router.post("/forgot-password", forgotPassword);
 router.post("/verify-otp", verifyOtp);
@@ -25,6 +29,7 @@ router.post("/reset-password", resetPassword);
 router.post("/verify-email", verifyEmail);
 router.post("/google", googleAuth);
 
+/* Protected Routes */
 router.post("/logout", protect, logout);
 router.get("/profile", protect, getProfile);
 router.put("/change-password", protect, changePassword);
