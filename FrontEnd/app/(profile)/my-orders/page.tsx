8ded949f41317580c98554496b2fa99c37e48e13
ZXMoahmed <mhmdalshwamy@outlook.com>
2026-05-04 21:22:22 +0200

    orders list + order products list

--- FrontEnd/app/(profile)/my-orders/page.tsx
+++ FrontEnd/app/(profile)/my-orders/page.tsx
@@ -1,5 +1,15 @@
+import ProfileCard from "../_components/ProfileCard";
+import OrderDetailsItem from "./_components/orderItem";
+
+
+
 const MyOrdersPage = () => {
-  return <div>MyOrdersPage</div>;
+  return (
+    <ProfileCard title="My Orders" subTitle="View and manage your orders">
+      <OrderDetailsItem data={{ date: "asdas", total: 13132, products: [{price:121,quantity:1, product: { id: "m", price: 12, name: "asd", images: ["/product1.png"] } }]}} currency="$" />
+      <OrderDetailsItem data={{ date: "asdas", total: 13132, products: [{price:121,quantity:1, product: { id: "m", price: 12, name: "asd", images: ["/product1.png"] } }]}} currency="$" />
+    </ProfileCard>
+  );
 };
 
 export default MyOrdersPage;
